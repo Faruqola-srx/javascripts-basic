6d0da37913fe6a7abbce61cb36d72d9b2b29706b
faruqola <faruqolamide142@gmail.com>
2025-01-30 06:43:59 -1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let firstName= 'Faruq';
+let secondName='olamide';
+console.log(firstName,secondName)
+//here i defined my name using javascript
+
+
+let age ='x'
+let x='32'
+console.log(x,age)
+/*defined a age using let
+x=the age which is 32*/
+
+let Firstname = 'ogunsola'
+    Lastname='olamide'
+console.log(Lastname,Firstname);
+
+const PI='3.14'
+console.log(PI)
+
+const l= 5;
+const m= 10;
+console.log(l+m);
+
+
+console.log('javascript is difficult')
+
+console.error('error error error')
+
+console.warn(warning);
+
+
+
+
+
+
+
+
